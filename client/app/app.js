a782de6ebf1e960b6fe90d14ae9c10d79bd520c1
Moncef12 <hasseinbey@gmail.com>
2015-10-02 01:04:21 +0100

    custom directive progress bar

--- client/app/app.js
+++ client/app/app.js
@@ -21,4 +21,59 @@ app.controller('phoneListCtrl', function($scope, $http){
 })
 
 
+app.directive('rtProgressBar', function(){
+    return {
+        scope: {
+            percentage:'=',
+            text: '@'
+        },
+        replace: true,
+        template: '<div>' +
+        '<div class="progress"> ' +
+            '<div class="progress-bar" role="progressbar" aria-valuenow="{{percentage}}" aria-valuemin="0" aria-valuemax="100" style="width: {{percentage}}%;"> ' +
+                '{{text +\':\'+  percentage}} %' +
+            '</div></div> <input id="edit-progress" ng-show="editProgress" type="text" ng-model="percentage"/><a href="#" ng-click="vm.toggleEditProgress()"></a>' +
+        '</div>',
+
+        link: link,
+        controller: controller,
+        controllerAs: 'vm'
+    }
+
+    function link(scope, element, attrs, ctrl){
+
+        angular.element(element[0].querySelector('.progress')).bind('click', function (e) {
+            if(scope.percentage < 100){
+                ctrl.toggleEditProgress();
+                // parceque, on a utilisé un evenement click standart, donc, on est hors
+                // angular contexte, on doit, lancer le $digest loop, avec $apply()
+                scope.$apply();
+            }
+        })
+
+        angular.element(element[0].querySelector('#edit-progress')).bind('blur', function (e) {
+            ctrl.toggleEditProgress();
+            // parceque, on a utilisé un evenement click standart, donc, on est hors
+            // angular contexte, on doit, lancer le $digest loop, avec $apply()
+            scope.$apply();
+        })
+    }
+
+    function controller($scope){
+
+        activate();
+
+        this.toggleEditProgress = toggleEditProgress;
+        ////////////////////
+        function activate(){
+            $scope.editProgress = false;
+        }
+
+        function toggleEditProgress(){
+            $scope.editProgress = ! $scope.editProgress;
+        }
+    }
+})
+
+
 
